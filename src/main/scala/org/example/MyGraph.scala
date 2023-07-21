c82db02d21af181f4197244a8e9611cb0a603959
JeffZ0930 <1937325843@qq.com>
2023-07-21 18:05:34 +0800

    Finished nodes(), rel(), and expand() API. Passed IS1 test.

--- src/main/scala/org/example/MyGraph.scala
+++ src/main/scala/org/example/MyGraph.scala
@@ -14,12 +14,109 @@ import org.opencypher.v9_0.expressions.SemanticDirection.{BOTH, INCOMING, OUTGOI
 import java.time.LocalDate
 import java.util.Date
 import java.sql.{Connection, DriverManager, ResultSet}
+import org.mongodb.scala._
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Projections._
+import org.mongodb.scala.model.Sorts._
+
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
 
 class MyGraph extends GraphModel {
 
-  private def transDate(date: Date): LocalDate = {
-    LocalDate.ofEpochDay(date.getTime/86400000)
+  val mongoClient = MongoClient("mongodb://localhost:27017")
+  val database = mongoClient.getDatabase("test")
+
+  private def myNodeAt(id: Int, tableList:Array[String]):Option[MyNode] = {
+    println("myNodeAt()")
+
+    for (tableName <- tableList) {
+      val collection = database.getCollection(tableName)
+      val findObservable = collection.find(equal("_id", id))
+      val docList = Await.result(findObservable.toFuture(), 10.seconds)
+
+      if (!docList.isEmpty) {
+        val result = rowToNode(docList.head, tableName, nodeSchema(tableName))
+
+        return Some(result)
+      }
+    }
+
+    None
   }
+
+  private def rowToNode(row: Document, tableName: String, config: Array[(String, String)]): MyNode = {
+    val propertyMap = config.indices.map { i =>
+      val columnName = config(i)._1
+      val columnType = config(i)._2
+      val rawValue = row.get(columnName)
+      val columnValue =
+        try {
+          columnType match {
+            case "BIGINT" => LynxValue(rawValue.get.asInt64().getValue)
+            case "INT" => LynxInteger(rawValue.get.asInt32().getValue)
+            case "String" => LynxString(rawValue.get.asString().getValue)
+            case _ => LynxString(rawValue.get.asString().getValue)
+          }
+        } catch {
+            case _: NullPointerException => LynxNull
+        }
+      LynxPropertyKey(columnName) -> columnValue
+    }.toMap
+
+    val id = MyId(row.get("_id").get.asInt32().getValue)
+    val label = Seq(LynxNodeLabel(tableName))
+    MyNode(id, label, propertyMap)
+  }
+
+  private def rowToRel(row: Document, tableName: String, config: Array[(String, String)]): MyRelationship = {
+    val propertyMap = config.indices.map { i =>
+      val columnName = config(i)._1
+      val columnType = config(i)._2
+      val rawValue = row.get(columnName)
+      val columnValue =
+        try {
+          columnType match {
+            case "BIGINT" => LynxValue(rawValue.get.asInt64().getValue)
+            case "INT" => LynxInteger(rawValue.get.asInt32().getValue)
+            case "String" => LynxString(rawValue.get.asString().getValue)
+            case _ => LynxString(rawValue.get.asString().getValue)
+          }
+        } catch {
+          case _: NullPointerException => LynxNull
+        }
+      LynxPropertyKey(columnName) -> columnValue
+    }.toMap
+
+    val id = MyId(row.get("_id").get.asInt32().getValue)
+    val startId = MyId(row.get("startId").get.asInt32().getValue)
+    val endId = MyId(row.get("endId").get.asInt32().getValue)
+
+    MyRelationship(id, startId, endId, Some(LynxRelationshipType(tableName)), propertyMap)
+  }
+
+  private val nodeSchema = Map(
+    "Person" -> Array(("_id", "INT"), ("LABEL", "String"), ("creationDate", "Date"), ("firstName", "String"),
+      ("lastName", "String"), ("gender", "String"), ("birthday", "Date"), ("locationIP", "String"), ("browserUsed", "String"),
+      ("languages", "String"), ("emails", "String")),
+    "Place" -> Array(("_id", "INT"), ("LABEL", "String"), ("name", "String"), ("url", "String"), ("type", "String")),
+  )
+
+  private val relSchema = Map(
+    "knows" -> Array(("_id", "INT"), ("LABEL", "String"), ("creationDate", "Date"), ("startId", "INT"), ("endId", "INT")),
+    "isLocatedIn" -> Array(("_id", "INT"), ("LABEL", "String"), ("startId", "INT"), ("endId", "INT"), ("creationDate", "Date")),
+  )
+
+  private val relMapping = Map(
+    "isLocatedIn" -> (Array("Person", "Comment", "Post", "Organisation"), Array("Place")),
+    "replyOf" -> (Array("Comment"), Array("Comment", "Post")), "containerOf" -> (Array("Forum"), Array("Post")),
+    "hasCreator" -> (Array("Comment", "Post"), Array("Person")), "hasInterest" -> (Array("Person"), Array("Tag")),
+    "workAt" -> (Array("Person"), Array("Organisation")), "hasModerator" -> (Array("Forum"), Array("Person")),
+    "hasTag" -> (Array("Comment", "Post", "Forum"), Array("Tag")), "hasType" -> (Array("Tag"), Array("Tagclass")),
+    "isSubclassOf" -> (Array("Tagclass"), Array("Tagclass")), "isPartOf" -> (Array("Place"), Array("Place")),
+    "likes" -> (Array("Person"), Array("Comment", "Post")), "knows" -> (Array("Person"), Array("Person")),
+    "studyAt" -> (Array("Person"), Array("Organisation")), "hasMember" -> (Array("Forum"), Array("Person")),
+  )
 
   override def write: WriteTask = new WriteTask {
     override def createElements[T](nodesInput: Seq[(String, NodeInput)], relationshipsInput: Seq[(String, RelationshipInput)], onCreated: (Seq[(String, LynxNode)], Seq[(String, LynxRelationship)]) => T): T = ???
@@ -53,17 +150,111 @@ class MyGraph extends GraphModel {
 
   override def nodeAt(id: LynxId): Option[MyNode] = ???
 
-  def myNodeAt(id: LynxId, tableList:Array[String]): Option[MyNode] = ???
-
   override def nodes(): Iterator[MyNode] = ???
 
-  override def nodes(nodeFilter: NodeFilter): Iterator[MyNode] = ???
+  override def nodes(nodeFilter: NodeFilter): Iterator[MyNode] = {
+    println("nodes(NodeFilter)")
+
+    if (nodeFilter.labels.isEmpty && nodeFilter.properties.isEmpty) {
+      return nodes()
+    }
+
+    val tableName = nodeFilter.labels.head.toString()
+    val collection = database.getCollection(tableName)
+
+    val filters = nodeFilter.properties.map { case (field, value) =>
+      equal(field.toString(), value.value)
+    }.toList
+
+    val findObservable = if (!filters.isEmpty) {
+      val finalFilter = and(filters: _*)
+      collection.find(finalFilter)
+    } else {
+      collection.find()
+    }
+
+    val documentsList = Await.result(findObservable.toFuture(), 10.seconds)
+
+    val result = documentsList.iterator.flatMap { doc =>
+      val node = rowToNode(doc, tableName, nodeSchema(tableName))
+      Iterator(node)
+    }
+
+    println("nodes(NodeFilter) finished")
+    result
+  }
 
   override def relationships(): Iterator[PathTriple] = ???
 
-  override def relationships(relationshipFilter: RelationshipFilter): Iterator[PathTriple] = ???
+  override def relationships(relationshipFilter: RelationshipFilter): Iterator[PathTriple] = {
+    println("relationships(RelFilter)")
 
-  override def expand(id: LynxId, filter: RelationshipFilter, direction: SemanticDirection): Iterator[PathTriple] = ???
+    val tableName = relationshipFilter.types.head.toString()
+    val collection = database.getCollection(tableName)
+
+    val filters = relationshipFilter.properties.map { case (field, value) =>
+      equal(field.toString(), value.value)
+    }.toList
+
+    val findObservable = if (!filters.isEmpty) {
+      val finalFilter = and(filters: _*)
+      collection.find(finalFilter)
+    } else {
+      collection.find()
+    }
+
+    val documentsList = Await.result(findObservable.toFuture(), 10.seconds)
+
+    val result = documentsList.iterator.flatMap { doc =>
+      val rel = rowToRel(doc, tableName, relSchema(tableName))
+      val startNode = myNodeAt(doc.get("startId").get.asInt32().getValue, relMapping(tableName)._1).get
+      val endNode = myNodeAt(doc.get("endId").get.asInt32().getValue, relMapping(tableName)._2).get
+      Iterator(PathTriple(startNode, rel, endNode))
+    }
+
+    println("relationships(RelFilter) finished")
+    result
+  }
+
+  override def expand(id: LynxId, filter: RelationshipFilter, direction: SemanticDirection): Iterator[PathTriple] = {
+    println("expand()")
+    if (direction == BOTH) {
+      return expand(id, filter, OUTGOING) ++ expand(id, filter, INCOMING)
+    }
+
+    val relType = filter.types.head.toString()
+    val startId = id.toLynxInteger.value.toInt
+    val startNode = direction match {
+      case OUTGOING => myNodeAt(startId, relMapping(relType)._1)
+      case INCOMING => myNodeAt(startId, relMapping(relType)._2)
+    }
+    if (startNode.isEmpty) {
+      return Iterator.empty
+    }
+
+    val filters = filter.properties.map { case (field, value) =>
+      equal(field.toString(), value.value)
+    }.toList :+ (direction match {
+      case OUTGOING => equal("startId", startId)
+      case INCOMING => equal("endId", startId)
+    })
+    val finalFilter = and(filters: _*)
+
+    val collection = database.getCollection(relType)
+    val findObservable = collection.find(finalFilter)
+    val documentsList = Await.result(findObservable.toFuture(), 10.seconds)
+
+    val result = documentsList.iterator.flatMap { doc =>
+      val endNode = direction match {
+        case OUTGOING => myNodeAt(doc.get("endId").get.asInt32().getValue, relMapping(relType)._2).get
+        case INCOMING => myNodeAt(doc.get("startId").get.asInt32().getValue, relMapping(relType)._1).get
+      }
+      Iterator(PathTriple(startNode.get, rowToRel(doc, relType, relSchema(relType)), endNode))
+    }
+
+    println("expand() finished")
+    result
+  }
 
   private val runner = new CypherRunner(this)
 
--- src/test/resources/LDBC/interactive-short-1.cypher
+++ src/test/resources/LDBC/interactive-short-1.cypher
@@ -3,16 +3,22 @@
 :param personId: 10995116277794
  */
 
-MATCH (n:Person {`id:ID`: $personId }) -[:isLocatedIn]-> (p:Place)
+MATCH (n:Person {`_id`: $personId }) -[:isLocatedIn]-> (p:Place)
 RETURN
     n.firstName AS firstName,
     n.lastName AS lastName,
     n.birthday AS birthday,
     n.locationIP AS locationIP,
     n.browserUsed AS browserUsed,
-    p.`id:ID` AS cityId,
+    p.`_id` AS cityId,
     n.gender AS gender,
-    n.creationDate AS creationDate
+    n.creationDate AS creationDat
+
+//MATCH (n:Person {`firstName`: $firstName }) -[:isLocatedIn]-> (p:Place)
+//RETURN
+//  n.firstName AS firstName,
+//  n.lastName AS lastName,
+//  p.`_id` AS cityId
 
 //MATCH (n:Person {`id:ID`: $personId }) -[:isLocatedIn]-> (p:Place) <-[:isLocatedIn]- (n2:Person)
 //RETURN
@@ -26,26 +32,18 @@ RETURN
 //    n2.gender AS gender,
 //    n2.creationDate AS creationDate
 
-//MATCH (p:Place {`id:ID`: $placeId}) <-[:isLocatedIn]- (n:Person)
+//MATCH (p:Place {`_id`: $placeId}) <-[:isLocatedIn]- (n:Person)
 //RETURN
-//  n.`id:ID` AS ID,
+//  n.`_id` AS ID,
 //  n.firstName AS firstName,
 //  n.lastName AS LastName
 
-//MATCH (n:Person) -[:isLocatedIn]-> (p:Place {`id:ID`: $placeId})
+//MATCH (n:Person) -[:isLocatedIn]-> (p:Place {`_id`: $placeId})
 //RETURN *
 
-//MATCH (n:Person {`id:ID`: $personId }) -[:isLocatedIn]-> (p:Place)
+//MATCH (p1:Person {`_id`: $personId }) <-[:knows]- (p2:Person)
 //RETURN
-//  n.`id:ID` as personId,
-//  n.firstName as firstName,
-//  n.lastName as lastName,
-//  p.`id:ID` as placeId,
-//  p.name as placeName
-
-//MATCH (p1:Person {`id:ID`: $personId }) <-[:knows]- (p2:Person)
-//RETURN
-//  p2.`id:ID` AS ID,
+//  p2.`_id` AS ID,
 //  p2.firstName AS firstName,
 //  p2.lastName AS lastName,
 //  p2.birthday AS birthday
